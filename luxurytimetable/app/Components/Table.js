3d7aad12b2ef237b9522dcc4cca4210e86961dcb
hanzla933 <hanzlahunny6@gmail.com>
2024-04-09 00:44:30 +0500

    style testing by hanzla

--- luxurytimetable/app/Components/Table.js
+++ luxurytimetable/app/Components/Table.js
@@ -42,28 +42,35 @@ const Table = (props) => {
       {timetable.map((form, formIndex) => (
         <form key={formIndex} onSubmit={(e) => handleFormSubmit(e, formIndex)}>
           {form.rows.map((row, rowIndex) => (
-            <div key={rowIndex}>
-              <label>Start Time</label>
+            <div key={rowIndex} className="table-row">
+              <label className="table-label bg-blue-500">Start Time</label>
               <input
                 type="time"
                 value={form.start}
                 onChange={(e) => handleFormChange(formIndex, 'start', e.target.value)}
+                className="table-input"
               />
-              <label>End Time</label>
+              <label className="table-label">End Time</label>
               <input
                 type="time"
                 value={form.end}
                 onChange={(e) => handleFormChange(formIndex, 'end', e.target.value)}
+                className="table-input"
               />
               {/* Add other inputs here */}
             </div>
           ))}
-          <button type="button" onClick={() => addRow(formIndex)}>Add Row</button>
-          <button type="submit">Submit</button>
+          <button type="button" onClick={() => addRow(formIndex)} className="add-row-button transition ease-in-out delay-150 bg-blue-600 hover:-translate-y-1 hover:scale-110 hover:bg-green-500 duration-300 ... flex mt-6 ml-20 w-20 text-center justify-center text-white rounded-md h-7 blur-none">
+            Add Row
+          </button>
+          <button type="submit" className="submit-button transition ease-in-out delay-150 bg-blue-600 hover:-translate-y-1 hover:scale-110 hover:bg-green-500 duration-300 ... flex mt-6 ml-20 w-20 text-center justify-center text-white rounded-md h-7 blur-none">
+            Submit
+          </button>
         </form>
       ))}
     </>
   );
+  
 };
 
 export default Table;
