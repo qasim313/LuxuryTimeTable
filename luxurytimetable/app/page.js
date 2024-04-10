e94ed31c2dba312d0d51dce52c1116fa320bb07d
Qasim Malik <qasimmalik808@gmail.com>
2024-04-10 05:23:05 +0500

    1 error resolved

--- luxurytimetable/app/page.js
+++ luxurytimetable/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState } from 'react';
 import SubjectCard from './components/SubjectCard';
 import PreferencesForm from './components/PreferencesForm';
 import TimetableDisplay from './components/TimetableDisplay';
